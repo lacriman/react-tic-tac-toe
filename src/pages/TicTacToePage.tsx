8c36ee8a94ca639720676251635372a320b7dde8
y.proskurin <y.proskurin@rto.de>
2026-07-14 13:36:25 +0200

    refactor: update TicTacToePage class names and adjust WinnerMessage styles

--- src/pages/TicTacToePage.tsx
+++ src/pages/TicTacToePage.tsx
@@ -3,7 +3,7 @@ import "./TicTacToePage.css";
 
 export function TicTacToePage() {
   return (
-    <section className="tic-tac-toe__wrapper">
+    <section className="tic-tac-toe">
       <h1 className="title">Tic-Tac-Toe</h1>
       <Board />
     </section>
